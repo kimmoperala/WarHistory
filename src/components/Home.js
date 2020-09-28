626b969ba3e59bccc4c2b78132c82575dd08c3d7
Samu <samu.rinne@metropolia.fi>
2020-09-28 16:48:01 +0300

    add some content to home page + css changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import "../customcss/mystylesheet.css"
 
 function Home(){
   return(
-      <h2>
-        ETUSIVU!
-      </h2>
+      <div className="homePage">
+        <h1>
+          Tervetuloa tutkimaan sotahistoriaa!
+        </h1>
+        <p>
+          Tällä sivustolla voit hakea tietoa vuosien 1400-2000 välillä käydyistä
+          sodista. Sotia voi hakea esimerkiksia alkamisajankohdalla tai sodan
+          maantieteellisellä sijainnilla.
+        </p>
+      </div>
   )
 }
 
